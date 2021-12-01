4791af02421f765378e27115c5b8ca03f0d2ef2d
Mikayla George <mikayla@hey.com>
2021-11-30 21:46:10 -0800

    add day 1 after cleanup

---
+++ day_1.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+require "colorize"
+
+DEBUG = false
+
+# a list of depths as read by sonar as we descend to the sea floor
+input = File.read("./inputs/day_1.txt")
+
+ary = input.split("\n").map do |string|
+    string.to_i
+end
+
+# part 1
+
+incs = 0
+last_measurement = -1
+ary.each do |measurement|
+    if last_measurement == -1
+        puts " ---- First Line!" if DEBUG
+    elsif measurement > last_measurement
+        incs += 1
+        puts "#{measurement} ---- Increased to #{incs} !" if DEBUG
+    else
+        puts "#{measurement} ---- Decreased." if DEBUG
+    end
+    last_measurement = measurement
+end
+
+puts "Part 1 answer: #{incs}"
+
+# part 2
+
+incs = 0
+last_measurement = -1
+
+ary.each.with_index do |measurement,index|
+    unless ary[index+2]
+        # last window
+        break
+    end
+    sum = ary[index] + ary[index+1] + ary[index+2]
+    print "#{ary[index]} + #{ary[index+1]} + #{ary[index+2]} = #{sum}" if DEBUG
+
+    if last_measurement == -1
+        puts " ---- First sum!" if DEBUG
+    elsif sum > last_measurement
+        incs += 1
+        puts " ---- Increased to #{incs}!" if DEBUG
+    else
+        puts " ---- Decreased." if DEBUG
+    end
+    last_measurement = sum
+end
+
+puts "Part 2 answer: #{incs}"
---
+++ readme.md
@@ -0,0 +1,3 @@
+this was a last-minute decision to participate, so it's not pretty!
+
+hope to make this prettier as I go!
