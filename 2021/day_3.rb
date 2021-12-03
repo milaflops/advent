baea13937efa316a5f48fee37c236cbc49e09695
Mikayla George <mikayla@hey.com>
2021-12-03 00:06:39 -0800

    clean up comments and add debug

--- 2021/day_3.rb
+++ 2021/day_3.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
 input = File.read("./inputs/day_3.txt")
-
 lines = input.split("\n")
+
+DEBUG = false
 
 # part 1
 
@@ -23,8 +24,8 @@ lines.each.with_index do |bin_line|
     total += 1
 end
 
-puts ones.inspect
-puts total
+puts ones.inspect if DEBUG
+puts total if DEBUG
 
 gamma_rate_bin = ones.map.with_index do |one,index|
     if ones[index] > (total / 2)
@@ -34,9 +35,9 @@ gamma_rate_bin = ones.map.with_index do |one,index|
     end
 end.join("")
 
-puts gamma_rate_bin
+puts "gamma rate binary: #{gamma_rate_bin}" # if DEBUG
 gamma_rate = gamma_rate_bin.to_i(2)
-puts gamma_rate
+puts gamma_rate if DEBUG
 
 epsilon_rate_bin = gamma_rate_bin.chars.map do |char|
     if char == "1"
@@ -48,7 +49,7 @@ end.join("")
 
 epsilon_rate = epsilon_rate_bin.to_i(2)
 
-puts epsilon_rate
+puts epsilon_rate if DEBUG
 
 part_1_answer = gamma_rate * epsilon_rate
 puts "Part 1 answer: #{part_1_answer}"
@@ -56,9 +57,9 @@ puts "Part 1 answer: #{part_1_answer}"
 # part 2
 
 def find_most_common(array, pos=0)
-    puts "    ---- "
-    puts "Finding most common in array of #{array.length}, pos #{pos}"
-    puts array.inspect
+    puts "    ---- " if DEBUG
+    puts "Finding most common in array of #{array.length}, pos #{pos}" if DEBUG
+    puts array.inspect if DEBUG
     number_of_zeros = 0
     number_of_ones = 0
     array.each do |line|
@@ -69,8 +70,8 @@ def find_most_common(array, pos=0)
         end
     end
 
-    puts "number of 0s: #{number_of_zeros}"
-    puts "number of 1s: #{number_of_ones}"
+    puts "number of 0s: #{number_of_zeros}" if DEBUG
+    puts "number of 1s: #{number_of_ones}" if DEBUG
 
     if number_of_zeros > number_of_ones
         most_common = "0"
@@ -92,9 +93,9 @@ def find_most_common(array, pos=0)
 end
 
 def find_least_common(array, pos=0)
-    puts "    ---- "
-    puts "Finding least common in array of #{array.length}, pos #{pos}"
-    puts array.inspect
+    puts "    ---- " if DEBUG
+    puts "Finding least common in array of #{array.length}, pos #{pos}" if DEBUG
+    puts array.inspect if DEBUG
     number_of_zeros = 0
     number_of_ones = 0
     array.each do |line|
@@ -113,8 +114,8 @@ def find_least_common(array, pos=0)
         least_common = "0"
     end
 
-    puts "number of 0s: #{number_of_zeros}"
-    puts "number of 1s: #{number_of_ones}"
+    puts "number of 0s: #{number_of_zeros}" if DEBUG
+    puts "number of 1s: #{number_of_ones}" if DEBUG
 
     reduced_array = array.select do |line|
         line[pos] == least_common
@@ -128,11 +129,11 @@ def find_least_common(array, pos=0)
 end
 
 oxygen_gen_bin = find_most_common(lines)
-puts oxygen_gen_bin
+puts oxygen_gen_bin if DEBUG
 oxygen_gen = oxygen_gen_bin.to_i(2)
 
 co2_scrubber_bin = find_least_common(lines)
-puts co2_scrubber_bin
+puts co2_scrubber_bin if DEBUG
 co2_scrubber = co2_scrubber_bin.to_i(2)
 
 life_support = oxygen_gen * co2_scrubber
