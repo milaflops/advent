574d55d6d665fd266be9b1d5868f7f4a083afa47
Mikayla George <mikayla@hey.com>
2021-12-13 00:47:47 -0800

    get part 1 working better

--- 2021/day_12.rb
+++ 2021/day_12.rb
@@ -73,62 +73,26 @@ hc-TR
 start-vh
 CAVES
 
-# class Cave
-#     attr_reader :name
-#     def initialize(string)
-#         @name = string
-#         if string == "start"
-#             @type = :start
-#         elsif string == "end"
-#             @type = :end
-#         elsif string == string.upcase
-#             @type = :big
-#         elsif string == string.downcase
-#             @type = :small
-#         end
-#         @connections = Set.new
-#     end
-
-#     def connect(cave)
-#         @connections.add(cave)
-#     end
-
-#     def inspect
-#         "cave: #{name} connections: [" +
-#         @connections.to_a.map do |connection|
-#             connection.name
-#         end.join(", ") + "]"
-#     end
-# end
+DEBUG = false
 
 def str_to_caves(string)
     cave_lookup = {}
     string.lines.each do |line|
         cave1, cave2 = line.strip.split("-")
-        # puts "#{cave1} ? no, #{cave2}"
-        # @cave1 = cave_lookup[cave1] || cave_lookup[cave1] = Cave.new(cave1)
-        # @cave2 = cave_lookup[cave2] || cave_lookup[cave2] = Cave.new(cave2)
-        # @cave1.connect(@cave2)
-        # @cave2.connect(@cave1)
         cave_lookup[cave1] ||= Set.new
         cave_lookup[cave1].add cave2
         cave_lookup[cave2] ||= Set.new
         cave_lookup[cave2].add cave1
     end
-    # puts cave_lookup.inspect
     cave_lookup
 end
 
 def caves_to_paths(caves)
     paths = []
     queue = [["start"]]
-    i = 0
     while !queue.empty?
-        # break if i > 50
         next_path = queue.shift
-        puts " ---- currently analyzing path: #{next_path.inspect}"
-        # record all traversed
-        # traversed_paths = Set.new
+        puts " ---- currently analyzing path: #{next_path.inspect}" if DEBUG
         visited = Set.new
         next_path.each do |stop|
             if stop == "start"
@@ -136,42 +100,40 @@ def caves_to_paths(caves)
             else
                 visited.add(stop)
             end
-
-            # next if index == 0
-            # traversed_paths.add("#{next_path[index-1]}-#{next_path}")
         end
         possibilities = caves[next_path.last].to_a
-        puts "  --- these are our possibilities: #{possibilities}"
+        puts "  --- these are our possibilities: #{possibilities}" if DEBUG
         possibilities.each do |possibility|
-            puts "   -- #{possibility}"
+            puts "   -- #{possibility}" if DEBUG
             if possibility == "end"
                 paths.push((next_path + [possibility]).join(","))
-                puts "finished path: #{paths.last}"
+                puts "finished path: #{paths.last}" if DEBUG
             elsif possibility == "start"
-                puts "not going back to start"
-                # do nothing
+                puts "not going back to start" if DEBUG
             elsif visited.include?(possibility)
-                puts "already visited #{possibility}"
-                # do nothing
-            # elsif traversed_paths.include?("#{next_path.last}-#{possibility}")
-            #     puts "already traversed #{next_path.last}-#{possibility}"
-            #     # do nothing
+                puts "already visited #{possibility}" if DEBUG
             else
-                # puts "our possibility is #{possibility}"
                 if possibility == possibility.downcase
                     visited.add(possibility)
                 end
                 queue.push(next_path + [possibility])
-                puts "pushed this one into queue"
-                # traversed_paths.add("#{next_path.last}-#{possibility}")
+                puts "pushed this one into queue" if DEBUG
             end
-            # visited.add(possibility.downcase)
         end
-        i += 1
     end
     paths
 end
 
-hash = str_to_caves(example1)
+[
+    ["example 1", example1, 10],
+    ["example 2", example2, 19],
+    ["example 3", example3, 226],
+    ["input", input, nil]
+].each do |name,cavestring,expected_paths|
+    hash = str_to_caves(cavestring)
+    paths = caves_to_paths(hash)
 
-puts caves_to_paths(hash)
+    puts " ---- #{name}"
+    puts "   -- expecting #{expected_paths} paths"
+    puts "   -- got #{paths.length} paths"
+end
